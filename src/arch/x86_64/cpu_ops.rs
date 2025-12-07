43bd1a8fd133262cf7e76055994030fb9e114c80
Chaseis4344 <townsendchase78@yahoo.com>
2025-12-07 08:41:21 -0500

    feat(partial): Additional Changes to add x86_64

--- src/arch/x86_64/cpu_ops.rs
+++ src/arch/x86_64/cpu_ops.rs
@@ -1,5 +1,30 @@
 use libkernel::CpuOps;
 
 impl CpuOps for super::x86_64 {
+    fn halt() -> ! {
+        loop {
+            unsafe{
+                naked_asm!{
+                    "hlt"
+                }
+            }
+        }
+    }
 
+    fn id() -> usize {
+       //set to zero and pretend there is only 1 until x86 instructions are figured out 
+        0
+    } 
+
+    fn disable_interupts() -> usize {
+
+    }
+
+    fn enable_interupts() {
+
+    }
+
+    fn restore_interupt_state(flags: usize) {
+
+    }
 }
---
+++ src/arch/x86_64/virtual_memory.rs
@@ -0,0 +1,5 @@
+use libkernel::VirtualMemory;
+
+impl VirtualMemory for x86_64 {
+
+}
