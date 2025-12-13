bf0142a9f1e11bd52ea35ad9e6a53925c522bfa3
Chaseis4344 <townsendchase78@yahoo.com>
2025-12-13 12:33:53 -0500

    feat(partial): LAHF Feature Check

--- src/arch/x86_64/cpu_ops.rs
+++ src/arch/x86_64/cpu_ops.rs
@@ -24,8 +24,17 @@ impl CpuOps for super::x86_64 {
             //
             // This isn't perfect but should work in theory
             // We will get a CPU exception if this instruction isn't supported for a CPU
-            asm! ("lahf", out("ah") interrupt_mask) //Get mask out
-            asm! ("cli") //Disable maskable interupts
+            let cpu_id_result: usize = 0;
+            
+            asm!("mov eax, 0x80000001");
+            asm!("cpuid", out("ecx") cpu_id_result);
+            if cpu_id_result == 1 {
+                asm!("lahf", out("ah") interrupt_mask); //Get mask out
+                asm!("cli"); //Disable maskable interupts
+            } else {
+                panic!("CPU does not support LAHF and SAHF")
+            }
+
         }
 
         interrupt_mask //return mask
@@ -40,7 +49,18 @@ impl CpuOps for super::x86_64 {
     fn restore_interupt_state(flags: usize) {
         unsafe {
             //Hoping that the asm macro loads registers before execution 
-            asm! ("sahf", in("ah") flags)
+            // TODO: Add a feature check for 64-bit mode to make sure it works
+            // and if it doesn't change lahf to use pushfd and popfd, and figure out how to
+            let cpu_id_result: usize = 0;
+            
+            asm!("mov eax, 0x80000001");
+            asm!("cpuid", out("ecx") cpu_id_result);
+            if cpu_id_result ==1 {
+                asm!("sahf", in("ah") flags)
+                asm!("sti")
+            }else {
+                panic!("CPU does not support LAHF and SAHF")
+            }
         }
     }
 }
