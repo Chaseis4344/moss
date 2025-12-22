1be479a043b815ae17c1fcc1ca3a1f513b66efcc
Chaseis4344 <townsendchase78@yahoo.com>
2025-12-22 10:07:25 -0500

    chore(minor): formatting & comments

--- src/arch/x86_64/mod.rs
+++ src/arch/x86_64/mod.rs
@@ -8,6 +8,15 @@ use libkernel::{
 mod cpu_ops;
 mod virtual_memory;
 
+fn extract_bit_to_bool(extract_from: u8, index: u8) -> bool {
+    let mut bitmask: u8 = 1;
+
+    bitmask = bitmask << index;
+    let num = (extract_from & bitmask) >> index;
+
+    num as bool
+}
+
 pub struct x86_64 {}
 impl Arch for x86_64 {
     fn name() -> &'static str {
