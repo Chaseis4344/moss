9d5c2b559a2259651c9a4a26bd77c239effcbef8
Chaseis4344 <townsendchase78@yahoo.com>
2025-12-26 15:39:26 -0500

    Revert "chore: Update Depends for x86_64"
    This reverts commit 2662561de12a3a3ae90cd5d7086b461f1a444b74.

--- src/arch/x86_64/mod.rs
+++ src/arch/x86_64/mod.rs
@@ -1,10 +1,9 @@
-use crate::arch::Arch;
+use super::Arch;
 use libkernel::{
     CpuOps, VirtualMemory,
     error::Result,
     memory::address::{UA, VA},
 };
-
 
 mod cpu_ops;
 mod virtual_memory;
@@ -20,67 +19,11 @@ fn extract_bit_to_bool(extract_from: u8, index: u8) -> bool {
 
 pub struct X86_64 {}
 impl Arch for X86_64 {
-
-    type UserContext = u8;
-    
     fn name() -> &'static str {
-        "x86-64"
+        "x86_64"
     }
 
     fn power_off() -> ! {
-        //Pull in things from acpi to accomplish
-        todo!("Arch Impl");
-    }
-
-    fn new_user_context(entry_point: VA, stack_top: VA) -> Self::UserContext	{
-        todo!("Arch Impl");
-    }
-    
-    fn context_switch(new: Arc<Task>)	{
-        todo!("Arch Impl");
-    }
-    
-    fn create_idle_task() -> Task	{
-        todo!("Arch Impl");
-    }
-    
-    fn restart() -> !	{
-        //Job for ACPI
-        todo!("Arch Impl");
-    }
-    
-    fn do_signal(
-        sig: SigId,
-        action: UserspaceSigAction,
-    ) -> impl Future<Output = Result<<Self as Arch>::UserContext>>	{
-        todo!("Arch Impl");
-    }
-
-    fn do_signal_return() -> impl Future<Output = Result<<Self as Arch>::UserContext>>	{
-        todo!("Arch Impl");
-    }
-
-    unsafe fn copy_from_user(src: UA, dst: *mut (), len: usize)
-    -> impl Future<Output = Result<()>>	{
-        todo!("Arch Impl");
-    }
-
-    unsafe fn try_copy_from_user(src: UA, dst: *mut (), len: usize) -> Result<()>	{
-        todo!("Arch Impl");
-    }
-
-    unsafe fn copy_to_user(src: *const (), dst: UA, len: usize)
-    -> impl Future<Output = Result<()>>	{
-        todo!("Arch Impl");
-    }
-
-    unsafe fn copy_strn_from_user(
-        src: UA,
-        dst: *mut u8,
-        len: usize,
-    ) -> impl Future<Output = Result<usize>>	{
-        //Can probably grab strn from libc and call it a day for x86
-        //after building out the interaction with userspace
-        todo!("Arch Impl");
+        loop {}
     }
 }
