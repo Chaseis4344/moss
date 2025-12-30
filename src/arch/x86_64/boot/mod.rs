f596960f595c9312f684ff2777bb94006817e5b2
Chaseis4344 <townsendchase78@yahoo.com>
2025-12-30 10:40:18 -0500

    feat: rudimentary grub2 boot

---
+++ src/arch/x86_64/boot/mod.rs
@@ -0,0 +1,11 @@
+
+
+//This will pull in everything and load into a multiboot long-mode enabled enviroment
+global_asm!(include_str!("start.S"))
+
+#[unsafe(no_mangle)]
+pub extern "C" fn arch_stage_3() {
+    //Should, again work in theory
+    todo!("Establish alloc, larger stack, and call kmain")
+
+} 
