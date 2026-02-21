497f1289c7d165277188bad7b56f20690a89d12e
Chaseis4344 <mctownsend2002@gmail.com>
2026-02-21 18:36:03 -0500

    feat: cpu_count and other improvements

--- src/arch/x86_64/virtual_memory.rs
+++ src/arch/x86_64/virtual_memory.rs
@@ -1,5 +1,35 @@
 use libkernel::VirtualMemory;
 use super::X86_64;
+use linked_list_allocator::Heap;
+
+use crate::sync::SpinLock;
+use core::{
+    alloc::{GlobalAlloc, Layout},
+    ptr::NonNull,
+};
+
+pub struct SpinlockHeap(pub SpinLock<Heap>);
+
+#[global_allocator]
+pub static HEAP_ALLOCATOR: SpinlockHeap = SpinlockHeap(SpinLock::new(Heap::empty()));
+
+unsafe impl GlobalAlloc for SpinlockHeap {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        self.0
+            .lock_save_irq()
+            .allocate_first_fit(layout)
+            .ok()
+            .map_or(core::ptr::null_mut(), |allocation| allocation.as_ptr())
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        unsafe {
+            self.0
+                .lock_save_irq()
+                .deallocate(NonNull::new_unchecked(ptr), layout)
+        }
+    }
+}
 
 impl VirtualMemory for X86_64 {
 
